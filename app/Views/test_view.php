a2706d1fe178f2a8d7a96baac2c5191c6792431e
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-06 21:15:13 +0700

    all commit

--- app/Views/test_view.php
+++ app/Views/test_view.php
@@ -364,7 +364,7 @@
                 console.log('Nilai dari message:', this.isDraw);
                 // Kode yang akan dijalankan setelah instance Vue di-mount
                 console.log('Vue instance has been mounted!');
-                const url = "<?= base_url('upload/engineering_doc/test.pdf') ?>";
+                const url = "<?= base_url('upload/engineering_doc/list/test.pdf') ?>";
 
                 function clearCanvas() {
                     canvas.clear();
