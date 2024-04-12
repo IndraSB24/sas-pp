b55e630f402002abb097dd3e10b35d48b9491bd9
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-12 16:20:01 +0700

    commit all

--- app/Views/master-data-karyawan.php
+++ app/Views/master-data-karyawan.php
@@ -40,7 +40,7 @@
                                         <div class="table-responsive">
                                             <table id="table_main" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                                 <thead>
-                                                    <tr>
+                                                    <tr class="bg-info">
                                                         <th>No.</th>
                                                         <th>Name</th>
                                                         <th>Email</th>
