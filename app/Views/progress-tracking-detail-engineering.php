857c31270f003c6a99068496366aa836da267a75
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-25 00:09:17 +0700

    commit all

--- app/Views/progress-tracking-detail-engineering.php
+++ app/Views/progress-tracking-detail-engineering.php
@@ -42,13 +42,6 @@
                                 <div class="card">
                                     <div class="card-body">
         
-                                        <!--<h4 class="card-title">Buttons Example</h4>-->
-                                        <!--<p class="card-title-desc">The Buttons extension for DataTables-->
-                                        <!--    provides a common set of options, API methods and styling to display-->
-                                        <!--    buttons on a page that will interact with a DataTable. The core library-->
-                                        <!--    provides the based framework upon which plug-ins can built.-->
-                                        <!--</p>-->
-        
                                         <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                             <thead>
                                                 <tr>
