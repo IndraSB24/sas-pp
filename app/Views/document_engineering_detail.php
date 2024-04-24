2af86d29c06546b84316c08d6b17b10981a57cc8
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-24 22:31:40 +0700

    commit all

--- app/Views/document_engineering_detail.php
+++ app/Views/document_engineering_detail.php
@@ -1117,7 +1117,7 @@
                     title: 'Diupload!',
                     icon: 'success',
                     text: 'File Berhasil Diupload.',
-                    timer: 500,
+                    timer: 1000,
                     confirmButtonColor: "#5664d2",
                     onBeforeOpen:function () {
                         //Swal.showLoading()
