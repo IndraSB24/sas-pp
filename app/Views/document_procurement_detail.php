90e73f868af0a209138c8c040c3b6d2a0393c8fd
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-25 00:53:51 +0700

    commit all

--- app/Views/document_procurement_detail.php
+++ app/Views/document_procurement_detail.php
@@ -60,11 +60,11 @@
                                                     </th>
                                                     <th colspan="3" class="text-center">MR RECIEVED DATE</th>
                                                     <th colspan="3" class="text-center">RFQ ISSUED DATE</th>
-                                                    <th cowspan="3" class="text-center">QUOTATION RECIEVED</th>
+                                                    <th colspan="3" class="text-center">QUOTATION RECIEVED</th>
                                                     <th colspan="3" class="text-center">TECHNICAL CLARIFICATION</th>
                                                     <th colspan="3" class="text-center">TBE ISSUED DATE</th>
-                                                    <th cowspan="3" class="text-center">CBE ISSUED DATE</th>
-                                                    <th cowspan="3" class="text-center">CONTRACT DATE</th>
+                                                    <th colspan="3" class="text-center">CBE ISSUED DATE</th>
+                                                    <th colspan="3" class="text-center">CONTRACT DATE</th>
                                                     <th cowspan="2" class="text-center">
                                                         ACTION
                                                     </th>
