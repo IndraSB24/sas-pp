ceafb9877a4fa6e2557d5da99bbf1dbabe9fbaaf
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-24 18:21:14 +0700

    commit all

--- app/Helpers/Wa_helper.php
+++ app/Helpers/Wa_helper.php
@@ -33,7 +33,7 @@
 	function waCoba($data=null){
 	    $dataWa['image']    = false;
 		$dataWa['phone']	= hostWa('1');
-		$dataWa['penerima']	= '085274897212';
+		$dataWa['penerima']	= '6285274897212';
 		$dataWa['pesan']	=   '*NOTIFIKASI SISTEM*'.
                                 '<br><br>*BERHASIL NGK TU*';
         sendPersonalV2Kudus($dataWa);
@@ -178,46 +178,21 @@
 	function sendPersonalV2($dataSend){
 	    $curl = curl_init();
         $token = $dataSend['devId'];
-        $random = true;
-        if($dataSend['image']){
-            $payload = [
-                "data" => [
-                    [
-                        'phone' => $dataSend['penerima'],
-                        'image' => $dataSend['image'],
-                        'caption' => $dataSend['caption'],
-                        'isGroup' => false
-                    ]
-                ]
-            ];
-        }else{
-            $payload = [
-                "data" => [
-                    [
-                        'phone' => $dataSend['penerima'],
-                        'message' => $dataSend['pesan'],
-                        'isGroup' => false
-                    ]
-                ]
-            ];   
-        }
+        $data = [
+        'phone' => '6281218xxxxxx',
+        'message' => 'hello there',
+        ];
         curl_setopt($curl, CURLOPT_HTTPHEADER,
             array(
                 "Authorization: $token",
-                "Content-Type: application/json"
             )
         );
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload) );
-        if($dataSend['image']){
-            curl_setopt($curl, CURLOPT_URL,  "https://jogja.wablas.com/api/v2/send-image");
-        }else{
-            curl_setopt($curl, CURLOPT_URL,  "https://jogja.wablas.com/api/v2/send-message");    
-        }
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_URL,  "https://kudus.wablas.com/api/send-message");
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        
         $result = curl_exec($curl);
         curl_close($curl);
         echo "<pre>";
