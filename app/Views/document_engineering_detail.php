d622f84cfea69f822c3de54f2def5d5abd8749ff
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-04-27 23:09:42 +0700

    commit all

--- app/Views/document_engineering_detail.php
+++ app/Views/document_engineering_detail.php
@@ -596,7 +596,7 @@ function generateWaitingBadge()
                                                     <tr>
                                                         <td nowrap style="border-left-width: 4px;"><?= $no ?></td>
                                                         <td nowrap style="border-left-width: 4px;"><?= $row->wbs_code ?></td>
-                                                        <td class="text-center" nowrap> <?= $row->doc_dicipline ?> </td>
+                                                        <td class="text-center" nowrap> <?= $row->level_code ?> </td>
                                                         <td><?= $row->description ?></td>
                                                         <td class="text-center" style="border-left-width: 4px;"><?= $actual_JEDHI ?></td>
                                                         <td class="text-center"><?= $enginerPP ?></td>
