5723a70cdfbb72fb61b40698ed5489dc480ad8e9
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-05-10 01:46:05 +0700

    commit all

--- app/Models/Model_doc_procurement.php
+++ app/Models/Model_doc_procurement.php
@@ -17,20 +17,14 @@ class Model_doc_procurement extends Model
         'id_project', 'activity_code', 'description',
         'activity_code', 
         
-        // 'description_of_work', 'mr_received_plan', 'mr_received_forecast', 
-        // 'mr_received_actual', 'rfq_issued_plan', 'rfq_issued_forecast', 'rfq_issued_actual', 
-        // 'quotation_received_plan', 'quotation_received_forecast', 'quotation_received_actual', 
-        // 'technical_clarification_plan', 'technical_clarification_forecast', 'technical_clarification_actual', 
-        // 'tbe_issued_plan', 'tbe_issued_forecast', 'tbe_issued_actual', 'cbe_issued_plan', 'cbe_issued_forecast', 
-        // 'cbe_issued_actual', 'contract_date_plan', 'contract_date_forecast', 'contract_date_actual',
-
         'activity_name_lvl_1', 'activity_name_lvl_2', 'activity_name_lvl_3', 'activity_name_lvl_4', 'detail_or_spesifikasi', 
         'quantity', 'unit', 'harga_satuan_material', 'total_harga_material', 'total_amount_material', 'harga_satuan_jasa', 
         'total_harga_jasa', 'total_amount_jasa', 'wf', 'po_plan', 'po_act', 'fat_plan', 'fat_act', 'rfs_plan', 'rfs_act', 
         'onsite_plan', 'onsite_act', 'install_plan', 'install_act', 'comm_plan', 'comm_act', 'id_group',
-        'po_filename', 'po_status', 'po_id_file', 'activity_name_lvl_5'
-
-        
+        'po_filename', 'po_status', 'po_id_file', 'activity_name_lvl_5',
+        'fat_filename', 'fat_status', 'fat_id_file', 'rfs_filename', 'rfs_status', 'rfs_id_file',
+        'onsite_filename', 'onsite_status', 'onsite_id_file', 'install_filename', 'install_status', 'install_id_file',
+        'comm_filename', 'comm_status', 'comm_id_file'
     ];
 
     protected $useTimestamps = true;
