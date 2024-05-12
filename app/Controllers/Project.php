27852995eff4c38494bc2d8246328faacfc13095
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-05-12 22:06:26 +0700

    commit all

--- app/Controllers/Project.php
+++ app/Controllers/Project.php
@@ -5,16 +5,21 @@ use App\Models\Model_project;
 use App\Models\Model_doc_engineering;
 use App\Models\Model_engineering_doc_comment;
 use App\Models\Model_engineering_doc_file;
+use App\Models\Model_data_helper;
+use App\Models\Model_week;
 
 class Project extends BaseController
 {
-    protected $main_model, $Model_doc_engineering, $Model_engineering_doc_comment, $Model_engineering_doc_file;
+    protected $main_model, $Model_doc_engineering, $Model_engineering_doc_comment, $Model_engineering_doc_file,
+		$Model_data_helper, $Model_week;
  
     function __construct(){
         $this->main_model = new Model_project();
 		$this->Model_doc_engineering = new Model_doc_engineering();
 		$this->Model_engineering_doc_comment = new Model_engineering_doc_comment();
 		$this->Model_engineering_doc_file = new Model_engineering_doc_file();
+		$this->Model_data_helper = new Model_data_helper();
+        $this->Model_week = new Model_week();
 		helper(['session_helper', 'upload_path_helper']);
     }
     
@@ -67,9 +72,38 @@ class Project extends BaseController
 	}
 	
 	public function show_over_prog_month_detail(){
+		// start of scurve data count engineering ===============================================
+        $getScurveDataPlan = $this->Model_doc_engineering->getScurveDataPlan();
+        $getScurveDataActual = $this->Model_doc_engineering->getScurveDataActual();
+
+        // count plan cum
+        $getScurveDataPlanCum = [];
+        $plan_cum_counted = 0;
+        foreach ($getScurveDataPlan as $key => $value) {
+            $plan_cum_counted += $value->cum_plan_wf;
+            $getScurveDataPlanCum[$key] = $plan_cum_counted;
+        }
+
+        // count act cum
+        $getScurveDataActualCum = [];
+        $actual_cum_counted = 0;
+        foreach ($getScurveDataActual as $key => $value) {
+            $actual_cum_counted += $value->cum_actual_wf;
+            $getScurveDataActualCum[$key] = $actual_cum_counted;
+        }
+        // end of scurve data ===================================================================
+		
 		$data = [
-			'title_meta' => view('partials/title-meta', ['title' => 'Progress by Month']),
-			'page_title' => view('partials/page-title', ['title' => 'Project', 'pagetitle' => 'Progress by Month'])
+			'title_meta' => view('partials/title-meta', ['title' => 'Progress by Week']),
+			'page_title' => view('partials/page-title', ['title' => 'Project', 'pagetitle' => 'Progress by Week']),
+			'subtitle' => 'Judul Project',
+            'dataWeek' => $this->Model_week->findAll(),
+            'scurveDataEngineering' => [
+                'dataPlan' => $getScurveDataPlan,
+                'dataActual' => $getScurveDataActual,
+                'dataPlanCum' => $getScurveDataPlanCum,
+                'dataActualCum' => $getScurveDataActualCum
+            ]
 		];
 		return view('project-dashboard-overal-prog-month', $data);
 	}
