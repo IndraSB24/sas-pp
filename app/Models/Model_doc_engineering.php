b017db91b646ed5f37f0d4cd8735c58843e836be
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-05-12 20:26:38 +0700

    commit all

--- app/Models/Model_doc_engineering.php
+++ app/Models/Model_doc_engineering.php
@@ -303,10 +303,6 @@ class Model_doc_engineering extends Model
                 project_detail_engineering pde3 ON (pde3.external_asbuild_actual BETWEEN dw.start_date AND dw.end_date)
             WHERE 
                 dw.start_date <= '$currentDate'
-            GROUP BY 
-                dw.id
-            ORDER BY 
-                dw.id
         ";
 
         $query = $this->db->query($sql);
