33c0b90ce9ccca6fbfcbc3b98f046d3a391217b4
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-05-19 21:05:28 +0700

    commit all

--- app/Models/Model_doc_engineering.php
+++ app/Models/Model_doc_engineering.php
@@ -1047,13 +1047,20 @@ class Model_doc_engineering extends Model
     
         // Iterate through each discipline
         foreach ($diciplineList as $value) {
+            $cumPlan = $this->getCumDataPlan($idProject, $value->id, true, null);
+            $cumActual = $this->getCumDataActual($idProject, $value->id, true, null);
+            $cumPlanCurrentWeek = $this->getCumDataPlan($idProject, $value->id, false, $currentWeek);
+            $cumActualCurrentWeek = $this->getCumDataActual($idProject, $value->id, false, $currentWeek);
+            $cumPlanLastWeek = $this->getCumDataPlan($idProject, $value->id, false, $lastWeek);
+            $cumActualLastWeek = $this->getCumDataActual($idProject, $value->id, false, $lastWeek);
+            
             $returnData[$value->name] = [
-                'cumPlan' => $this->getCumDataPlan($idProject, $value->id, true, null),
-                'cumActual' => $this->getCumDataActual($idProject, $value->id, true, null),
-                'cumPlanCurrentWeek' => $this->getCumDataPlan($idProject, $value->id, false, $currentWeek),
-                'cumActualCurrentWeek' => $this->getCumDataActual($idProject, $value->id, false, $currentWeek),
-                'cumPlanLastWeek' => $this->getCumDataPlan($idProject, $value->id, false, $lastWeek),
-                'cumActualLastWeek' => $this->getCumDataActual($idProject, $value->id, false, $lastWeek)
+                'cumPlan' => $cumPlan[0],
+                'cumActual' => $cumActual[0],
+                'cumPlanCurrentWeek' => $cumPlanCurrentWeek[0],
+                'cumActualCurrentWeek' => $cumActualCurrentWeek[0],
+                'cumPlanLastWeek' => $cumPlanLastWeek[0],
+                'cumActualLastWeek' => $cumActualLastWeek[0]
             ];
         }
     
