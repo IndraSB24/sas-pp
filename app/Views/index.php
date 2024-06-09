f96e2a2786e5442a66c3d30cca61e24306eb9479
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-06-09 22:05:32 +0700

    commit all

--- app/Views/index.php
+++ app/Views/index.php
@@ -295,7 +295,7 @@
 
 <script>
 
-    console.log(<?= json_encode($session_data) ?>, 'SESSION DATA');
+    console.log(<?= json_encode(sess('active_karyawan_id')) ?>, 'SESSION DATA');
 
 
     // datatable
