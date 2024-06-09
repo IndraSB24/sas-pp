78cdf1cff667c7d985d7cfe89811df22d920a2d4
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-06-09 23:13:07 +0700

    commit all

--- app/Models/Model_doc_procurement.php
+++ app/Models/Model_doc_procurement.php
@@ -56,7 +56,7 @@ class Model_doc_procurement extends Model
     }
 
     // get all man hour by dicipline per month
-    public function getAll(){
+    public function getAll_1(){
         $this->select('
             project_detail_procurement.*,
             dh.name as group_name
@@ -68,20 +68,20 @@ class Model_doc_procurement extends Model
         return $this->get()->getResult();
     }
 
-    public function getAll_1($id_karyawan=null) {
+    public function getAll($id_karyawan=null) {
         if($id_karyawan != null){
             $this->select('
-                project_detail_engineering.*,
+                project_detail_procurement.*,
                 dh.name as doc_dicipline,
                 kdr.id_doc_role as procurement_role
             ')
-            ->join('data_helper dh', 'dh.id = project_detail_engineering.id_doc_dicipline', 'LEFT')
+            ->join('data_helper dh', 'dh.id=project_detail_procurement.id_group', 'LEFT')
             ->join('karyawan_doc_role kdr', 
                 'kdr.id_karyawan = ' . $this->db->escape($id_karyawan) . ' AND kdr.doc_type = "procurement"', 
                 'LEFT'
             )
-            ->where('project_detail_engineering.deleted_at', NULL)
-            ->orderBy('project_detail_engineering.id');
+            ->where('project_detail_procurement.deleted_at', NULL)
+            ->orderBy('project_detail_procurement.id');
 
         }else{
             $this->select('
