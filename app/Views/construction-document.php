827695fe9e0a2accd67b669ec9a815f82b666566
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-03 22:02:40 +0700

    all

--- app/Views/construction-document.php
+++ app/Views/construction-document.php
@@ -840,6 +840,7 @@
 <script src="<?= base_url('assets/libs/select2/js/select2.min.js') ?>"></script>
 
 <script>
+    console.log(<?= json_encode($list_doc) ?>, 'LIST DOC');
     console.log(<?= json_encode($scurveData) ?>, 'scurveData');
     console.log(<?= json_encode($progressChartData) ?>, 'CHART PROGRESS');
     console.log(<?= json_encode($getProgressByLevel1ForChart) ?>, 'getProgressByLevel1ForChart');
