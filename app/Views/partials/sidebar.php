67c59df809d289b05180f94e2a091cb5fab2a06f
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-06 21:05:49 +0700

    all

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -24,18 +24,20 @@
                 </li>
 
                 <!-- list project -->
-                <?php //$sidebar_data = get_data_list_project(); ?>
+                <?php $sidebar_data = get_data_list_project(); ?>
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-file-list-line"></i>
                         <span>Project List</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="<?= base_url('entitas-usaha-get-list') ?>">Data Entitas Usaha</a></li>
-                        <li><a href="<?= base_url('item-get-list') ?>">Data Produk</a></li>
-                        <li><a href="<?= base_url('satuan-get-list') ?>">Data Satuan</a></li>
-                        <li><a href="<?= base_url('paymentMethod-get-list') ?>">Data Metode Bayar</a></li>
-                        <li><a href="<?= base_url('distributionChanel-get-list') ?>">Data Chanel Distribusi</a></li>
+                        <?php foreach($sidebar_data as $list): ?>
+                            <li>
+                                <a href="<?= base_url('entitas-usaha-get-list') ?>">
+                                    <?= $list->contract_no ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
                     </ul>
                 </li>
 
